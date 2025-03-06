6a97c898a30b262e0d36b1c9ab93db7fd6caa801
Anshi Patel <anshipatel881@gmail.com>
2025-03-06 15:53:53 +0530

    [Anshi] Add . Added user input validation for name field

--- Helloworld/src/app/app.component.ts
+++ Helloworld/src/app/app.component.ts
@@ -13,11 +13,20 @@ export class AppComponent {
   imgUrl = "assets/bridgelabzlogo.png";
   url = "https://www.bridgelabz.com";
   userName : string ="";
+  nameError: string ="";
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz.";
   }
   onClick($event:Event){
     console.log("Save button is clicked!", $event);
     window.open(this.url,"_blank");
+  }
+  onInput($event:Event){
+    console.log("change Even Occurered!",this.userName);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.nameError="";
+      return;
+    }this.nameError="Name is Incorrect";
   }
 }
